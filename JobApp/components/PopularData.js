dcbea0a3a2e240e8b18c7e9b8f3d84ea06e0f417
Dwamenachrist <agyapongfrancis520@gmail.com>
2024-06-18 15:24:26 +0400

    Component for Popular Job cars

---
+++ JobApp/components/PopularData.js
@@ -0,0 +1,54 @@
+const Popular = [
+	{
+		Jobtitle: "Jr Executive",
+		company: "Burger King",
+		logo: require("../assets/burger.png"),
+		salary: "$96,000/y",
+		location: "Los Angels, US",
+	  },
+	  {
+		Jobtitle: "Product Manager",
+		company: "Beats",
+		logo: require("../assets/beats.png"),
+		salary: "$84,000/y",
+		location: "Florida, US",
+	  },
+	  {
+		Jobtitle: "Product Manager",
+		company: "Facebook",
+		logo: require("../assets/facebook2.png"),
+		salary: "$84,000/y",
+		location: "Florida, US",
+	  },
+	  {
+		Jobtitle: "Marketing Specialist",
+		company: "Nike",
+		logo: require("../assets/nike.png"),
+		salary: "$75,000/y",
+		location: "Oregon, US",
+	  },
+	  {
+		Jobtitle: "Software Engineer",
+		company: "Apple",
+		logo: require("../assets/apple.png"),
+		salary: "$110,000/y",
+		location: "California, US",
+	  },
+	  {
+		Jobtitle: "Data Analyst",
+		company: "LinkedIn",
+		logo: require("../assets/linkedin.png"),
+		salary: "$90,000/y",
+		location: "California, US",
+	  },
+	  {
+		Jobtitle: "UX Designer",
+		company: "Pinterest",
+		logo: require("../assets/pinterest.png"),
+		salary: "$85,000/y",
+		location: "California, US",
+	  },
+  ];
+  
+  export default Popular;
+  
